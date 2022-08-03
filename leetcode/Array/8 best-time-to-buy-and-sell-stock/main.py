86964710da1e4ce9bb0553f673e6817c7444a9ad
Shaon Majumder <smazoomder@gmail.com>
2022-08-04 01:06:02 +0600

    On

--- leetcode/Array/8 best-time-to-buy-and-sell-stock/main.py
+++ leetcode/Array/8 best-time-to-buy-and-sell-stock/main.py
@@ -2,22 +2,19 @@
 # refernce https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
 class Solution:
     def maxProfit(self, prices):
-        prices_size = len(prices)
-        if prices_size > 1:
-            max_diff = prices[1] - prices[0]
+        length = len(prices)
+        if length > 1:
             min_element = prices[0]
+            max_diff = prices[1] - min_element
             
-            for i in range( 1, prices_size ):
-                if (prices[i] - min_element > max_diff):
+            for i in range(1,length):
+                if prices[i] - min_element > max_diff:
                     max_diff = prices[i] - min_element
-            
-                if (prices[i] < min_element):
+                if prices[i] < min_element:
                     min_element = prices[i]
             return max_diff if max_diff >= 0 else 0
         else:
-            return 0
-
-        
+            return 0       
 
 s = Solution()
 # print( s.maxProfit([7,6,4,3,1]) )
