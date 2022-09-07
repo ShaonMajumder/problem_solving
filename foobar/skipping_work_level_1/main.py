ae7041bdae14e6598ab8a9b7b6ac6bde97983d9d
Shaon Majumder <smazoomder@gmail.com>
2022-09-07 16:12:26 +0600

    Level 1 completed

---
+++ foobar/skipping_work_level_1/main.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+def solution(x, y):
+    def helper(subset, superset):
+        dic = defaultdict(lambda : 0)
+
+        for i in subset:
+            dic[i] += 1
+
+        for i in superset:
+            dic[i] -= 1
+            if dic[i] < 0:
+                return i
+
+    if len(x) > len(y):
+        return helper(y,x)
+    else:
+        return helper(x,y)
+    
+
+print( solution([4,5,6,6,6], [4,5,6,6]) )
